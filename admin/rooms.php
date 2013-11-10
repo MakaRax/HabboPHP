1d74fe33658380f0efa798a18fbdc1f27d931cb2
Nicoluc2 <kirchhoffer.nicolas@live.fr>
2013-11-10 14:17:48 +0000

    Adding room page

---
+++ admin/rooms.php
@@ -0,0 +1,65 @@
+<?php include 'includes/header.php'; 
+if(!Tools::checkACL($user->rank, ACL_ROOM_DEFAULT_REGISTER)){
+	header("Location: /admin/");
+	die();
+}
+?>
+<header class="jumbotron subhead" id="overview">
+  <h1>Apparts</h1>
+  <p class="lead">Rechercher des apparts, modifier l'appart de base à l'inscription</p>
+  <div class="subnav">
+   <ul class="nav nav-pills">
+      <?php if(Tools::checkACL($user->rank, ACL_ROOM_DEFAULT_REGISTER)){ ?><li><a href="#">Construire un appart par défaut</a></li><?php } ?>
+  </ul>
+  </div>
+</header>
+<section id="search">
+	<div class="form-horizontal">
+        <fieldset>
+        	<?php if(isset($success)){ echo '<div class="alert alert-success">Appart de base modifié !</div>'; } ?>
+        	<form action="#" method="post">
+			<input type="text" class="" style="width:99%;font-size:24px;height:40px;" placeholder="Identifiant de l'appart à mettre par défaut" name="id">       
+          	<div class="form-actions" style="text-align:center;">
+            <input type="submit" style="margin-left:-154px;font-size:24px;" class="btn btn-primary btn-large" value="Mettre par défaut" />
+          </div>
+      </form>
+        </fieldset>
+        <?php
+        	if(isset($_POST['id'])){
+        		$room = mysql_query("SELECT * FROM rooms WHERE id = '".safe(intval($_POST['id']), 'SQL')."'");
+        		$r = mysql_fetch_assoc($room);
+        		$first = '<room id="default">
+	<roomdata>
+		<roomtype>private</roomtype>
+		<caption>'.$r['caption'].'</caption>
+		<model_name>'.$r['model_name'].'</model_name>
+		<description>'.$r['description'].'</description>
+	</roomdata>';
+				$first .= '<roomitem>';
+				$item = mysql_query("SELECT base_item, x, y, z, rot FROM items WHERE room_id = '".safe(intval($_POST['id']), 'SQL')."'");
+				$it = mysql_fetch_assoc($item);
+				while($i = $it){
+					$first .= '<item>
+			<base_item>'.$i['base_item'].'</base_item>
+			<x>'.$i['x'].'</x>
+			<y>'.$i['y'].'</y>
+			<z>'.$i['z'].'</z>
+			<rot>'.$i['rot'].'</rot>
+		</item>';
+				}
+				$first .= "</roomitem>";
+				$first .= "</room>";
+				$f = "../modules/rooms/default.xml";
+				if(is_writable($f)){
+					$open = fopen($f, "w");
+					fwrite($open, $first);
+					fclose($open);
+					$success = true; 
+				} else {
+					echo "<div class=\"alert alert-error\">Le fichier /modules/rooms/default.xml n'est pas écrivable (chmod < 777). Vous pouvez tout de même l'écrire à la main avec le code disponible ci-dessous.</div>";
+					echo "<div class=\"well\">".htmlentities($first)."</div>";
+				}				
+        	}
+        ?>
+      </div>
+</section>
